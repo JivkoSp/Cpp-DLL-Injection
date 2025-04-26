66a3d2c28e23ad0b35595e69187bf4c54571f733
JivkoSp <atjspasov@gmail.com>
2025-04-26 23:16:29 +0300

    Update

--- x1ct/x1ct_app/x1ct_app.cpp
+++ x1ct/x1ct_app/x1ct_app.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <tchar.h>
+#include <wchar.h>
+#include <windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    HMODULE hDll = LoadLibraryW(_T("x1ct"));
+
+    if (!hDll || hDll == INVALID_HANDLE_VALUE)
+    {
+        std::cout << "Invalid handle!\n";
+        return 1;
+    }
+
+    return 0;
 }
