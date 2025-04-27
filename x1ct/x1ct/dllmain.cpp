058851f223d1402a05f827289fce987a8b3a533f
JivkoSp <atjspasov@gmail.com>
2025-04-27 22:10:20 +0300

    Update

--- x1ct/x1ct/dllmain.cpp
+++ x1ct/x1ct/dllmain.cpp
@@ -1,6 +1,14 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include "framework.h"
 #include <iostream>
+
+EXTERN_C void CallAx12(void)
+{
+    std::cout << "CallAx12 called!\n";
+
+    system("start notepad.exe");
+}
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -10,16 +18,12 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-        std::cout << "PROCESS ATTACH\n";
+        CallAx12();
+        return TRUE;
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
     }
     return TRUE;
-}
-
-EXTERN_C void CallAx12(void)
-{
-    std::cout << "CallAx12 called!\n";
 }
--- x1ct/x1ct_app/x1ct_app.cpp
+++ x1ct/x1ct_app/x1ct_app.cpp
@@ -2,28 +2,93 @@
 #include <tchar.h>
 #include <wchar.h>
 #include <windows.h>
+#include <TlHelp32.h>
+
+#define DLL_PATH "D:\\Uni\\C++\\Projects\\DLLInjection\\x1ct\\x64\\Debug\\x1ct.dll"
+#define TARGET_BINARY L"Notepad.exe"
 
 typedef int(__stdcall* CallAx12)();
 
+DWORD findProccessID();
+
 int main()
 {
-    HMODULE hDll = LoadLibraryW(_T("x1ct"));
-   
-    if (!hDll || hDll == INVALID_HANDLE_VALUE)
+    DWORD pid;
+    HANDLE hProcess;
+    LPVOID lpBaseAddress;
+    size_t sz = strlen(DLL_PATH);
+    int output_val = 0;
+    
+    std::cout << "BEGIN\n";
+
+    // get pid for target binary
+    pid = findProccessID();
+
+    // get handle to target proccess
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
+
+    // allocate memory to target proccess
+    lpBaseAddress = VirtualAllocEx(hProcess, NULL, sz, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+    if (lpBaseAddress == 0)
     {
-        std::cout << "Invalid handle!\n";
         return 1;
     }
 
-    CallAx12 callAx12 = (CallAx12)GetProcAddress(hDll, "CallAx12");
+    // write the DLL path to the target binary memory
+    output_val = WriteProcessMemory(hProcess, lpBaseAddress, DLL_PATH, sz, NULL);
 
-    if (!callAx12)
+    // get habdle to kernel32.dll
+    HMODULE hModule = GetModuleHandle(L"kernel32.dll");
+
+    if (hModule == 0)
     {
-        std::cout << "Invalid function call!\n";
         return 1;
     }
+    // get address of LoadLibraryA
+    LPVOID lpStartAddress = GetProcAddress(hModule, "LoadLibraryA");
 
-    callAx12();
+    // create remote thread in target binary
+    HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)lpStartAddress, lpBaseAddress, 0, NULL);
+
+    if (hThread)
+    {
+        std::cout << "Successful injection!";
+    }
+    else
+    {
+        std::cout << "Injection faild!";
+    }
+
+    return 0;
+}
+
+DWORD findProccessID()
+{
+    HANDLE hProcessSnap;
+    PROCESSENTRY32 pe32;
+    DWORD dwPriorityClass;
+
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    if (!Process32First(hProcessSnap, &pe32))
+    {
+        CloseHandle(hProcessSnap);
+
+        return FALSE;
+    }
+
+    do
+    {
+        if (!wcscmp(pe32.szExeFile, TARGET_BINARY))
+        {
+            return pe32.th32ProcessID;
+        }
+
+    } while (Process32Next(hProcessSnap, &pe32));
+
 
     return 0;
 }
